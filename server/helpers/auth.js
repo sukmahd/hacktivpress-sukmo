e51d7b6f46293197ecb4acd84bb81653b2b88065
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-08-15 14:20:05 +0700

    auth login

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -25,15 +25,7 @@ function hash(data, key){
 function auth(req,res,next){
   const token = req.headers.token
   if(token){
-    jwt.verify(token, 'AssFF9', function(err, decode){
-      console.log(decode);
-      console.log(req.params.id);
-      if(req.params.id == decode.id){
-        next();
-      }else {
-        res.send('u dont have access to this account')
-      }
-    })
+    next()
   }else {
     res.send('anda belum login')
   }
--- server/routes/articles.js
+++ server/routes/articles.js
@@ -3,12 +3,13 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/articleController')
+const middleware = require('../helpers/auth')
 
 router.get('/', controller.getAllArticles)
 router.get('/:id', controller.getOneArticle)
-router.post('/', controller.postArticle)
-router.delete('/:id', controller.deleteArticle)
-router.put('/:id', controller.editArticle)
+router.post('/', middleware.auth, controller.postArticle)
+router.delete('/:id',  middleware.auth, controller.deleteArticle)
+router.put('/:id',  middleware.auth, controller.editArticle)
 router.get('/category/:category', controller.getByCategory)
 router.get('/author/:author', controller.getByAuthor)
 
